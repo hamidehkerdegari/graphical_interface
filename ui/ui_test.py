3993fb8bde5035d093fc2a55c5a4c26db013664a
saeid <saeid.mokaram@gmail.com>
2017-08-09 13:10:39 +0100

    v1

--- ui/ui_test.py
+++ ui/ui_test.py
@@ -6,47 +6,58 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 # ==============================================================
 
-def img_plot(axes, image, size, pos, zorder, ofset_centre=True):
-    if ofset_centre:
-        extent = [pos[0] - (size[0] / 2), pos[0] + (size[0] / 2), \
-                  pos[1] - (size[1] / 2), pos[1] + (size[1] / 2) ]
-    else:
-        extent = [pos[0], pos[0] + size[0], pos[1], pos[1] + size[1]]
+class MiroGI():
+    def __init__(self):
+        rospy.init_node("miro_ros_client_py", anonymous=True)
+        self.miro = lib.miro_ros_client()
 
-    img_handle = axes.imshow(image, zorder=zorder, extent=extent)
-    return img_handle
+        Back_image = plt.imread('../documents/biomimetic_core.png')
+        self.emoji_1 = plt.imread('../documents/happy.png')
 
-def makw_GI(Back_image, screen_size):
-    fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(9, 5))
-    plt.subplots_adjust(left=0.0, right=1.0, top=1.0, bottom=0.0, wspace=0.0, hspace=0.0)
-    manager = plt.get_current_fig_manager()
-    manager.resize(*manager.window.maxsize())
+        self.screen_size = [800, 450]
+        self.fig, self.axes = plt.subplots(nrows=1, ncols=1, figsize=(9, 5))
+        plt.subplots_adjust(left=0.0, right=1.0, top=1.0, bottom=0.0, wspace=0.0, hspace=0.0)
+        self.pltManager = plt.get_current_fig_manager()
+        self.pltManager.resize(*self.pltManager.window.maxsize())
 
-    # Setting the back/static image(s).
-    axes.imshow(Back_image, zorder=0, extent=[0, screen_size[0], 0, screen_size[1]])
-    axes.axis('off')  # clear x- and y-axes
-    axes.set_xlim([0, screen_size[0]])
-    axes.set_ylim([0, screen_size[1]])
+        # Setting the back/static image(s).
+        self.axes.imshow(Back_image, zorder=0, extent=[0, self.screen_size[0], 0, self.screen_size[1]])
+        self.axes.axis('off')  # clear x- and y-axes
+        self.axes.set_xlim([0, self.screen_size[0]])
+        self.axes.set_ylim([0, self.screen_size[1]])
 
-    return fig, axes
+        # Initializing moving images.
+        self.emoji_handle = self.axes.imshow(self.emoji_1, zorder=1, extent=[0, 30, 0, 30])
 
-def plot_updatefig(i):
-    if rospy.core.is_shutdown():
-        return
-    while miro.platform_sensors is None:
-        pass
-    miro.update_data()
+        # cursor = Cursor()
+        cid = self.fig.canvas.mpl_connect('button_press_event', self.onclick)
 
-    # Plotting animation
-    global emoji_handle
-    emoji_handle.remove()
-    x = miro.accel_head.x
-    y = miro.accel_head.y
-    emoji_handle = img_plot(axes, emoji_1, [10, 10], [(x*100)+354, (y*100)+101], 1)
-# ==============================================================
+        ani = animation.FuncAnimation(self.fig, self.plot_updatefig, interval=500)
+        plt.show()
 
+    def img_plot(self, image, size, pos, zorder, ofset_centre=True):
+        if ofset_centre:
+            extent = [pos[0] - (size[0] / 2), pos[0] + (size[0] / 2), \
+                      pos[1] - (size[1] / 2), pos[1] + (size[1] / 2) ]
+        else:
+            extent = [pos[0], pos[0] + size[0], pos[1], pos[1] + size[1]]
 
-class Cursor():
+        img_handle = self.axes.imshow(image, zorder=zorder, extent=extent)
+        return img_handle
+
+    def plot_updatefig(self, i):
+        if rospy.core.is_shutdown():
+            return
+        while self.miro.platform_sensors is None:
+            pass
+        self.miro.update_data()
+
+        # Plotting animation
+        self.emoji_handle.remove()
+        x = self.miro.accel_head.x
+        y = self.miro.accel_head.y
+        self.emoji_handle = self.img_plot(self.emoji_1, [10, 10], [(x*100)+354, (y*100)+101], 1)
+
     # Getting the cursor click position.
     def onclick(self, event):
         print('button=%d, x=%d, y=%d, xdata=%f, ydata=%f' %
@@ -54,22 +65,7 @@ class Cursor():
 
 ################################################################
 if __name__ == "__main__":
-    rospy.init_node("miro_ros_client_py", anonymous=True)
-    miro = lib.miro_ros_client()
-
-    Back_image = plt.imread('../documents/biomimetic_core.png')
-    emoji_1 = plt.imread('../documents/happy.png')
-
     # Setting the back/static image(s).
-    fig, axes = makw_GI(Back_image, [800, 450])
-
-    cursor = Cursor()
-    cid = fig.canvas.mpl_connect('button_press_event', cursor.onclick)
-
-    # Initializing moving images.
-    emoji_handle = axes.imshow(emoji_1, zorder=1, extent=[0, 30, 0, 30])
-
-    ani = animation.FuncAnimation(fig, plot_updatefig, interval=500)
-    plt.show()
+    MainGI = MiroGI()
 
 ################################################################
